bb9f7a9df790615b9a39f515fd15b7fa791fc98d
KRishika01 <156343658+KRishika01@users.noreply.github.com>
2025-07-27 19:10:55 +0530

    Update ping.c

--- ping.c
+++ ping.c
@@ -1,11 +1,11 @@
 #include "ping.h"
 
-int pid_in_ping = -1;
-int ctrlC_flag = 0;
+int pid_in_ping = -1;    // to track a process that should receive SIGINT when Ctrl+C is pressed
+int ctrlC_flag = 0;      // to indicate if Ctrl+C was pressed, though in this version it's not used
 int fg_running = 0;
 // int returnCode_in_bg_fg;
 
-
+/* It checks if there's a foreground process (pid_in_ping != -1). If yes, it sends SIGINT to that process. If not, it simply prints a newline and refreshes the shell prompt.*/
 void ctrlC_execution() {
     if(pid_in_ping != -1) {
         kill(pid_in_ping,SIGINT);
@@ -17,6 +17,7 @@ void ctrlC_execution() {
     }
 }
 
+/*If a background process is being tracked (returnCode_in_bg_fg > 0), and it’s not in the foreground (fg_running == 0), it is forcefully killed using SIGKILL. Then the shell prints a logout message and exits safely.*/
 void ctrlD_execution() {
     if(returnCode_in_bg_fg > 0 && fg_running == 0) {
         kill(returnCode_in_bg_fg,SIGKILL);
